9d65662246614db738d8629389e83eb708b0cb2a
George <george.irving1@gmail.com>
2021-10-21 21:03:26 +0100

    No longer requires last year's code. This is last commit with code left in. Next commit it will be removed.

--- PyCharm project/RDF.py
+++ PyCharm project/RDF.py
@@ -26,6 +26,35 @@ def rdf_setup(xyz_file_path):
 
     return length_one_unit_cell, xyz_array_float_stacked
 
+
+def rdf_setup_2(xyz_file_path):
+
+    with open(xyz_file_path, "r") as xyz:
+        num_xyz = xyz.readline()
+        mof_name = xyz.readline()
+
+        xyz_array = np.array(xyz.readline().split(), ndmin=2)
+
+        for line in xyz:
+            array_to_join = np.array(line.split(), ndmin=2)
+            xyz_array = np.concatenate((xyz_array, array_to_join), axis=0)
+
+    xyz_array_float = xyz_array[:, 1:4].astype(np.double)
+
+    length_xyz_array_float = xyz_array_float.shape[0]
+    length_one_unit_cell = int(length_xyz_array_float / 27)
+
+    xyz_array_float_stacked = np.array(xyz_array_float[0:27, :], ndmin=3)
+    xyz_array_float_stacked = np.swapaxes(xyz_array_float_stacked, 0, 1)
+    for t in range(1, length_one_unit_cell):
+        array_to_join = np.array(xyz_array_float[t*27:(t+1)*27, :], ndmin=3)
+        array_to_join = np.swapaxes(array_to_join, 0, 1)
+        xyz_array_float_stacked = np.concatenate((xyz_array_float_stacked, array_to_join), axis=1)
+
+    return length_one_unit_cell, xyz_array_float_stacked
+
+
+
 @jit(nopython=True, parallel=True)
 def rdf_single_R(length_one_unit_cell, xyz_array_float_stacked, R, B, Pi, Pj):
 
@@ -47,6 +76,8 @@ def rdf_single_R(length_one_unit_cell, xyz_array_float_stacked, R, B, Pi, Pj):
         #     print(i)
     return sum_RDF
 
+
+
 @jit(nopython=True, parallel=True)
 def rdf(RDF,length_one_unit_cell, xyz_array_float_stacked, Rs, B, Pi, Pj):
 
@@ -58,14 +89,14 @@ def rdf(RDF,length_one_unit_cell, xyz_array_float_stacked, Rs, B, Pi, Pj):
     return RDF
 
 Rs = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
-
+#Rs = [2]
 
 #Makes a typed.List as lists are deprecating in Numba
 typed_Rs = List()
 [typed_Rs.append(x) for x in Rs]
 
 RDF = np.empty([len(typed_Rs), 2])
-length_one_unit_cell, xyz_array_float_stacked = rdf_setup("./.RASPA_Output/zns222.xyz")
+length_one_unit_cell, xyz_array_float_stacked = rdf_setup_2("./.RASPA_Output/zns222.xyz")
 RDF = rdf(RDF,length_one_unit_cell, xyz_array_float_stacked, typed_Rs, 10, 1, 1)
 
 
