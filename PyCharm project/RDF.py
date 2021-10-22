e201e1233e0c714effb27a4c58f89ed7fe86db0a
George <george.irving1@gmail.com>
2021-10-23 00:25:53 +0100

    rdf_setup2 created. On next commit will remove rdf_setup. rdf_setup2 uses jit. Also, attempted to add timing. This will likely be removed on next commit.

--- PyCharm project/RDF.py
+++ PyCharm project/RDF.py
@@ -1,8 +1,20 @@
 import numpy as np
-from numba import jit, prange
-from time import time
+from numba import jit, prange, objmode
+import time
 
-def rdf_setup(xyz_file_path):
+
+def rdf_load_xyz(xyz_file_path):
+
+    xyz_array = np.loadtxt(xyz_file_path, dtype=str,skiprows=2)
+    xyz_array_float = xyz_array[:, 1:4].astype(float)
+
+    length_xyz_array_float = xyz_array_float.shape[0]
+    length_one_unit_cell = int(length_xyz_array_float / 27)
+
+    return xyz_array_float, length_one_unit_cell
+
+#@jit(nopython=True, parallel=True)
+def rdf_setup(xyz_array_float, length_one_unit_cell):
 
     # with open(xyz_file_path, "r") as xyz:
     #     num_xyz = xyz.readline()
@@ -14,13 +26,6 @@ def rdf_setup(xyz_file_path):
     #         array_to_join = np.array(line.split(), ndmin=2) ##<== the slow step is np.array
     #         xyz_array = np.concatenate((xyz_array, array_to_join), axis=0)
 
-    xyz_array = np.loadtxt(xyz_file_path, dtype=str,skiprows=2)
-
-    xyz_array_float = xyz_array[:, 1:4].astype(np.double)
-
-    length_xyz_array_float = xyz_array_float.shape[0]
-    length_one_unit_cell = int(length_xyz_array_float / 27)
-
     xyz_array_float_stacked = np.array(xyz_array_float[0:27, :], ndmin=3)
     xyz_array_float_stacked = np.swapaxes(xyz_array_float_stacked, 0, 1)
 
@@ -31,45 +36,28 @@ def rdf_setup(xyz_file_path):
 
     return length_one_unit_cell, xyz_array_float_stacked
 
+@jit(nopython=True, parallel=True)
+def rdf_setup2(xyz_array_float, xyz_array_float_stacked, length_one_unit_cell):
 
+    for t in prange(0, length_one_unit_cell):
+        array_to_join = xyz_array_float[t*27:(t+1)*27, :]
+        xyz_array_float_stacked[:,t,:] = array_to_join
+
+    return xyz_array_float_stacked
 
 @jit(nopython=True, parallel=True)
-def rdf_single_R(length_one_unit_cell, xyz_array_float_stacked, R, B, Pi, Pj):
-
-    sum_RDF = 0
+def rdf(RDF, length_one_unit_cell, xyz_array_float_stacked, Rs, B, Pi, Pj):
+    with objmode(start_time='f8'):
+        start_time = time.perf_counter()
+    printer = False
 
     for i in prange(0, length_one_unit_cell):
-        for j in prange(0, length_one_unit_cell):
-            if j > i:
-                r = 1000
-                for k in range(0, 27):
-                #for k in range(13, 14):
-                    euclid_dist = np.linalg.norm(xyz_array_float_stacked[13, i, 0:3] - xyz_array_float_stacked[k, j, 0:3])
-                    if euclid_dist < r:
-                        r = euclid_dist
-                summand = Pi * Pj * np.exp(-B * ((r - R) ** 2))
-                sum_RDF += summand
 
-        # if i%100 == 0:
-        #      print(i)
-    return sum_RDF
+        if i % int(length_one_unit_cell / 10) == 0:
+            with objmode(inner_start_time='f8'):
+                inner_start_time = time.perf_counter()
+            printer = True
 
-
-
-@jit(nopython=True, parallel=True)
-def rdf(RDF,length_one_unit_cell, xyz_array_float_stacked, Rs, B, Pi, Pj):
-    for R_index in prange(0, len(Rs)):
-        RDF_single_R=rdf_single_R(length_one_unit_cell, xyz_array_float_stacked, Rs[R_index], 10, 1, 1)
-        RDF[R_index,0] = Rs[R_index]
-        RDF[R_index,1] = RDF_single_R
-        print(R_index)
-    return RDF
-
-
-
-@jit(nopython=True, parallel=True)
-def rdf2(RDF, length_one_unit_cell, xyz_array_float_stacked, Rs, B, Pi, Pj):
-    for i in prange(0, length_one_unit_cell):
         for j in prange(0, length_one_unit_cell):
             if j > i:
                 r = 1000
@@ -83,7 +71,17 @@ def rdf2(RDF, length_one_unit_cell, xyz_array_float_stacked, Rs, B, Pi, Pj):
                 for R_index in range(0, len(Rs)):
                     R = Rs[R_index]
                     summand = Pi * Pj * np.exp(-B * ((r - R) ** 2))
-                    RDF[R_index] = RDF[R_index] + summand
+                    RDF[R_index, 1] = RDF[R_index,1] + summand
+                    RDF[R_index, 0] = R
+
+        if printer:
+            printer = False
+            with objmode():
+                inner_end_time = time.perf_counter()
+                total_time_estimate = (inner_end_time-inner_start_time)*length_one_unit_cell
+                total_time_passed = inner_end_time-start_time
+                print("Estimated total time: ", total_time_estimate, "Time passed:", total_time_passed)
+
 
 
     return RDF
--- PyCharm project/main.py
+++ PyCharm project/main.py
@@ -3,31 +3,44 @@ from RDF import *
 import numpy as np
 from numba.typed import List
 import time
+
 def main():
 
-    mof_name = "MIL-47"
+    mof_name = "ZIF-20"
 
     print("######### Converting .cif to .xyz")
     run_cif_to_xyz_diffpy(mof_name)
 
     print("######### Successfully converted .cif to .xyz")
 
-    #Rs = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+    #Rs = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 265, 266]
     # Rs = [2]
-    Rs = np.linspace(2, 30, num=10)
+    Rs = np.linspace(2, 30, num=100)
+    #Rs = np.array([265, 266])
 
     # Makes a typed.List as lists are deprecating in Numba
     typed_Rs = List()
     [typed_Rs.append(x) for x in Rs]
 
-    print("######### Calculating RDF")
+    #s = time.time()
 
-    s = time.time()
-    RDF = np.empty([len(typed_Rs), 2])
-    length_one_unit_cell, xyz_array_float_stacked = rdf_setup("./"+mof_name+".xyz")
+    print("######### RDF Calculations - Loading .xyz")
+    xyz_array_float, length_one_unit_cell = rdf_load_xyz("./"+mof_name+".xyz")
+
+    #Create empty stacked xyz
+    xyz_array_float_stacked = np.zeros([27, length_one_unit_cell, 3])
+
+    print("######### RDF Calculations - Stacking adjacent unit cells")
+    xyz_array_float_stacked = rdf_setup2(xyz_array_float, xyz_array_float_stacked, length_one_unit_cell)
+
+    # Create empty RDF array
+    RDF = np.zeros([len(typed_Rs), 2])
+
+    print("######### RDF Calculations - Calculating RDF")
     RDF = rdf(RDF, length_one_unit_cell, xyz_array_float_stacked, typed_Rs, 10, 1, 1)
-    f = time.time()
-    print(f-s)
+
+    #f = time.time()
+    #print(f-s)
     print("######### Successfully calculated RDF")
 
     return RDF
