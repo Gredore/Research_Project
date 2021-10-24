07926784bad1054475f400ef36d6a539691e84bd
George <george.irving1@gmail.com>
2021-10-24 14:58:35 +0100

    Implimented property vectors

--- PyCharm project/RDF.py
+++ PyCharm project/RDF.py
@@ -1,7 +1,16 @@
 import numpy as np
 from numba import jit, prange, objmode
+import math
 import time
 import sys
+
+
+### These two functions are used to encode the element name into an int that can be parsed to numba functions (unlike str)
+def convertStrToNumber(s):
+    return int.from_bytes(s.encode(), 'little')
+
+def convertStrFromNumber(n):
+    return n.to_bytes(math.ceil(n.bit_length() / 8), 'little').decode()
 
 def rdf_load_xyz(xyz_file_path):
 
@@ -11,24 +20,34 @@ def rdf_load_xyz(xyz_file_path):
     length_xyz_array_float = xyz_array_float.shape[0]
     length_one_unit_cell = int(length_xyz_array_float / 27)
 
-    atoms = np.array([ord(atom_i) for atom_i in xyz_array[:, 0]])
+    atoms = np.array([convertStrToNumber(atom_i) for atom_i in xyz_array[:, 0]])
     atoms = np.transpose(atoms)
+    # Convert strings to floats in the atoms array
+    atoms = atoms.astype(np.float)
 
-    return xyz_array_float, length_one_unit_cell, atoms
+    all_element_property_vectors = np.loadtxt('Property_vectors.csv',dtype=str, delimiter=',', skiprows=1)
+    all_element_property_vectors[:, 0] = np.transpose(np.array([convertStrToNumber(element_i) for element_i in all_element_property_vectors[:, 0]]))
+    # Convert strings to floats in the element property vector array
+    all_element_property_vectors = all_element_property_vectors.astype(np.float)
+
+    return xyz_array_float, length_one_unit_cell, atoms, all_element_property_vectors
 
 @jit(nopython=True, parallel=True)
-def rdf_setup(xyz_array_float, xyz_array_float_stacked, length_one_unit_cell, unit_cell_atoms, atoms):
+def rdf_setup(xyz_array_float, xyz_array_float_stacked, length_one_unit_cell, unit_cell_atoms, atoms, all_element_property_vectors, all_unit_cell_property_vectors):
 
     for t in prange(0, length_one_unit_cell):
         array_to_join = xyz_array_float[t*27:(t+1)*27, :]
         xyz_array_float_stacked[:,t,:] = array_to_join
         atom_to_join = atoms[t*27]
         unit_cell_atoms[t] = atom_to_join
+        all_element_property_vectors_atoms = all_element_property_vectors[:, 0]
+        all_unit_cell_property_vectors[t] = all_element_property_vectors[np.where(all_element_property_vectors_atoms == atom_to_join)[0][0],1:np.shape(all_element_property_vectors)[1]]
 
-    return xyz_array_float_stacked, unit_cell_atoms
+    return xyz_array_float_stacked, unit_cell_atoms, all_unit_cell_property_vectors
+
 
 @jit(nopython=True, parallel=True)
-def rdf(RDF, length_one_unit_cell, xyz_array_float_stacked, Rs, B, Pi, Pj):
+def rdf(RDF, length_one_unit_cell, xyz_array_float_stacked, Rs, B, property_vector):
     print("######### RDF Calculations - 10 chunk complete messages expected:")
     for i in prange(0, length_one_unit_cell):
 
@@ -44,7 +63,7 @@ def rdf(RDF, length_one_unit_cell, xyz_array_float_stacked, Rs, B, Pi, Pj):
 
                 for R_index in range(0, len(Rs)):
                     R = Rs[R_index]
-                    summand = Pi * Pj * np.exp(-B * ((r - R) ** 2))
+                    summand = property_vector[i] * property_vector[j] * np.exp(-B * ((r - R) ** 2))
                     RDF[R_index, 1] = RDF[R_index,1] + summand
                     RDF[R_index, 0] = R
 
--- PyCharm project/main.py
+++ PyCharm project/main.py
@@ -4,7 +4,7 @@ import numpy as np
 from numba.typed import List
 import time
 
-def main(mof_name, num_sample_rs):
+def main(mof_name, num_sample_rs, property_index):
 
     print("######### Converting .cif to .xyz")
     run_cif_to_xyz_diffpy(mof_name)
@@ -20,20 +20,22 @@ def main(mof_name, num_sample_rs):
     #s = time.time()
 
     print("######### RDF Calculations - Loading .xyz")
-    xyz_array_float, length_one_unit_cell, atoms = rdf_load_xyz("./"+mof_name+".xyz")
+    xyz_array_float, length_one_unit_cell, atoms, all_element_property_vectors = rdf_load_xyz("./"+mof_name+".xyz")
 
-    #Create empty stacked xyz
+    # Create empty stacked xyz
     xyz_array_float_stacked = np.zeros([27, length_one_unit_cell, 3])
     # Create empty unit cell atom list
     unit_cell_atoms = np.empty(shape=[length_one_unit_cell, 1])
+    # Create empty unit cell property vector
+    all_unit_cell_property_vectors = np.zeros(shape=[length_one_unit_cell,np.shape(all_element_property_vectors)[1]-1])
     print("######### RDF Calculations - Stacking adjacent unit cells")
-    xyz_array_float_stacked, unit_cell_atoms = rdf_setup(xyz_array_float, xyz_array_float_stacked, length_one_unit_cell, unit_cell_atoms, atoms)
+    xyz_array_float_stacked, unit_cell_atoms, all_unit_cell_property_vectors = rdf_setup(xyz_array_float, xyz_array_float_stacked, length_one_unit_cell, unit_cell_atoms, atoms, all_element_property_vectors, all_unit_cell_property_vectors)
 
     # Create empty RDF array
     RDF = np.zeros([len(typed_Rs), 2])
 
     print("######### RDF Calculations - Calculating RDF")
-    RDF = rdf(RDF, length_one_unit_cell, xyz_array_float_stacked, typed_Rs, 10, 1, 1)
+    RDF = rdf(RDF, length_one_unit_cell, xyz_array_float_stacked, typed_Rs, 10, all_unit_cell_property_vectors[:,property_index])
 
     #f = time.time()
     #print(f-s)
@@ -42,5 +44,5 @@ def main(mof_name, num_sample_rs):
     return RDF
 #if __name__ == "__main__":
 
-RDF = main("MIL-47", 300)
+RDF = main("MIL-47", 300, 0)
 #Fourier_RDF = np.real(np.fft.rfft(RDF[:,1], axis=0))
