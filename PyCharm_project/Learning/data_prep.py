30975a25526aad70fca0fa9e6c3d6d60322f7def
George <george.irving1@gmail.com>
2022-01-29 17:23:29 +0000

    Most RDFs made. Learning codewritten but currently model is not producing results better than the weightings in the input data.

--- PyCharm_project/Learning/data_prep.py
+++ PyCharm_project/Learning/data_prep.py
@@ -1,13 +1,14 @@
 import numpy as np
 from tensorflow.keras.utils import to_categorical
+from sklearn.utils import class_weight
 
 def data_prep():
     #Configure the preparation of the training set data
     RDF_Outputs_path = "../../RDF_Outputs/"
     r_spacing = 0.1
     largest_r = 60
-    successful_s = [1,3,5,6,7,8,9,11,12,13,14,15,16,18,19,20]
-    successful_u = [1, 2, 4, 5, 6, 7, 8, 9, 10]
+    successful_s = [1,3,5,6,7,8,9,11,12,13,14,15,16,18,19,20,21,22,23,24,27,28,29,30,31,32,33,34,35,37,38,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,99,101,103,104,105,106,107,108]
+    successful_u = [1,2,4,5,6,7,8,9,10,11,12,13,14,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42]
 
     num_stable_RDFs = len(successful_s)
     num_unstable_RDFs = len(successful_u)
@@ -44,9 +45,11 @@ def data_prep():
         #Unstable is defined as 0
         y_train_RDF[i] = 0
 
+    class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(y_train_RDF), y=y_train_RDF)
+    class_weights_dict = dict(enumerate(class_weights))
     y_train_RDF = to_categorical(y_train_RDF)
 
-    return X_train_RDF, y_train_RDF
+    return X_train_RDF, y_train_RDF, class_weights_dict, num_stable_RDFs, num_unstable_RDFs
 
 
 
--- PyCharm_project/Learning/learning_main.py
+++ PyCharm_project/Learning/learning_main.py
@@ -2,10 +2,77 @@ import numpy as np
 from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense, Conv1D, Flatten
+from keras.callbacks import EarlyStopping
+import sklearn
+from sklearn.model_selection import KFold
+from matplotlib import pyplot as plt
 
 from data_prep import *
 
+num_folds = 2
+epochs = 150
+early_stopping_patience = 5000
 
 #NOTE: Configuration MUST be done within data_prep.py before running!
-X_train_RDF, y_train_RDF = data_prep()
+X_train_RDF, y_train_RDF, class_weights_dict, num_stable_RDFs, num_unstable_RDFs = data_prep()
 
+
+# Define per-fold score containers
+acc_per_fold = []
+loss_per_fold = []
+
+kfold = KFold(n_splits=num_folds, shuffle=True)
+
+#val_accuracy_summed = np.zeros([1, epochs])
+
+fold_no = 1
+for train, test in kfold.split(X_train_RDF, y_train_RDF):
+
+    model = Sequential()
+
+    model.add(Conv1D(512, kernel_size=3, activation='relu', input_shape=([X_train_RDF.shape[1], 1])))
+    model.add(Conv1D(128, kernel_size=3, activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(30, activation='softmax'))
+    model.add(Dense(2, activation='softmax'))
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    print(f'Training for fold {fold_no} ...')
+
+    es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=early_stopping_patience)
+
+    history = model.fit(X_train_RDF[train], y_train_RDF[train],validation_data=(X_train_RDF[test], y_train_RDF[test]),class_weight=class_weights_dict, epochs=epochs, verbose=1, callbacks=[es])
+
+
+    #val_accuracy_summed = val_accuracy_summed + np.array(history.history['val_accuracy'])
+
+    # Generate generalization metrics
+    scores = model.evaluate(X_train_RDF[test], y_train_RDF[test], verbose=0)
+    print(f'Score for fold {fold_no}: {model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1] * 100}%')
+    acc_per_fold.append(scores[1] * 100)
+    loss_per_fold.append(scores[0])
+
+    fold_no = fold_no + 1
+
+print('------------------------------------------------------------------------')
+print('Score per fold')
+for i in range(0, len(acc_per_fold)):
+    print('------------------------------------------------------------------------')
+    print(f'> Fold {i + 1} - Loss: {loss_per_fold[i]} - Accuracy: {acc_per_fold[i]}%')
+print('------------------------------------------------------------------------')
+print('Average scores for all folds:')
+print(f'> Accuracy: {np.mean(acc_per_fold)} (+- {np.std(acc_per_fold)/num_folds})')
+print(f'> Loss: {np.mean(loss_per_fold)}')
+print('------------------------------------------------------------------------')
+print("Weighted random guessing would give accuracy: ", num_stable_RDFs/(num_stable_RDFs+num_unstable_RDFs))
+# plt.plot(history.history['accuracy'])
+# plt.plot(history.history['val_accuracy'])
+# plt.plot(val_accuracy_summed)
+# plt.title('model accuracy')
+# plt.ylabel('accuracy')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'val'], loc='upper left')
+
+#plt.plot(val_accuracy_summed[0,:])
+#plt.show()
